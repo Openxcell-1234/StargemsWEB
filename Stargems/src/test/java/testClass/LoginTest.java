c2d981ab5d84762fd20c21336fd425a513319455
Openxcell-1234 <161296995+Openxcell-1234@users.noreply.github.com>
2024-02-27 13:12:29 +0530

    Commit new code-8

---
+++ Stargems/src/test/java/testClass/LoginTest.java
@@ -0,0 +1,17 @@
+package testClass;
+
+import org.testng.annotations.Test;
+
+public class LoginTest extends BeforeAfterTest{
+	
+	@Test
+	public void method() {
+		
+	}
+	
+	@Test
+	public void method_1() {
+		
+	}
+
+}
