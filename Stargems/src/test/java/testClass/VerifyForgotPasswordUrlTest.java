27e499c5d375e64d803af91c3122c3d17c529b5a
Openxcell-1234 <161296995+Openxcell-1234@users.noreply.github.com>
2024-02-27 13:07:59 +0530

    Commit new code-1

--- Stargems/src/test/java/testClass/VerifyForgotPasswordUrlTest.java
+++ Stargems/src/test/java/testClass/VerifyForgotPasswordUrlTest.java
@@ -10,11 +10,14 @@ import pageClass.VerifyForgotPasswordPage;
 public class VerifyForgotPasswordUrlTest extends BeforeAfterTest{
 	
 	@Test
-	public void VerifyForgotPasswordUrlTestMethod() {
+	public void VerifyForgotPasswordUrlTestMethod() throws Exception {
 		VerifyAndDeleteRegisteredAccountPage homePageObj = VerifyAndDeleteRegisteredAccountPage.objectReturn();
 		homePageObj.clickOnUserIcon();
+		
+		Thread.sleep(10000);
 		VerifyForgotPasswordPage objForgorPwd = VerifyForgotPasswordPage.objectReturn();
 		objForgorPwd.clickOnForgotPassword();
+		Thread.sleep(15000);
 		
 		String actualUrl = objForgorPwd.getCurrentUrlMethod();
 		String expectedUrl = "https://stargems-frontend.apps.openxcell.dev/forgot-password";
