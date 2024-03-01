827c10b5e7054a550440538971f83f3025436ea6
Openxcell-1234 <161296995+Openxcell-1234@users.noreply.github.com>
2024-03-01 15:28:02 +0530

    new xyz commit

--- Stargems/src/main/java/pageClass/VerifyForgotPasswordPage.java
+++ Stargems/src/main/java/pageClass/VerifyForgotPasswordPage.java
@@ -8,22 +8,6 @@ import testBase.TestBase;
 
 public class VerifyForgotPasswordPage extends TestBase{
 	
-	@FindBy(xpath = "//a[@href= '/forgot-password']") private WebElement forgotPassword;
 	
-	public VerifyForgotPasswordPage() {
-		PageFactory.initElements(driver, this);
-	}
-	
-	public void clickOnForgotPassword() {
-		forgotPassword.click();
-	}
-	
-	public String getCurrentUrlMethod() {
-		return driver.getCurrentUrl();
-	}
-	
-	public static VerifyForgotPasswordPage objectReturn() {
-		return new VerifyForgotPasswordPage();
-	}
 
 }
--- Stargems/src/test/java/testClass/VerifyForgotPasswordUrlTest.java
+++ Stargems/src/test/java/testClass/VerifyForgotPasswordUrlTest.java
@@ -1,27 +1,13 @@
 package testClass;
 
-import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import pageClass.VerifyAndDeleteRegisteredAccountPage;
-import pageClass.VerifyForgotPasswordPage;
 
 
 public class VerifyForgotPasswordUrlTest extends BeforeAfterTest{
 	
 	@Test
 	public void VerifyForgotPasswordUrlTestMethod() throws Exception {
-		VerifyAndDeleteRegisteredAccountPage homePageObj = VerifyAndDeleteRegisteredAccountPage.objectReturn();
-		homePageObj.clickOnUserIcon();
-		
-		Thread.sleep(10000);
-		VerifyForgotPasswordPage objForgorPwd = VerifyForgotPasswordPage.objectReturn();
-		objForgorPwd.clickOnForgotPassword();
-		Thread.sleep(5000);
-		
-		String actualUrl = objForgorPwd.getCurrentUrlMethod();
-		String expectedUrl = "https://stargems-frontend.apps.openxcell.dev/forgot-password";
-		Assert.assertEquals(actualUrl, expectedUrl);
+	
 	}
 
 }
